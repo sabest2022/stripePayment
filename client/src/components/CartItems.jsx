6cbe206700f5a9cec00da8cebb6c7674c4293a7e
sabest2022 <askarians@me.com>
2023-09-08 01:37:43 +0200

    UX debugning

--- client/src/components/CartItems.jsx
+++ client/src/components/CartItems.jsx
@@ -26,20 +26,30 @@ function Products() {
 
 
 
-
     return (
-        <div>
-            <h1>Products</h1>
-            {products.map(product => (
-                <div key={product.id}>
-                    <h2>{product.name}</h2>
-                    <p>{product.description}</p>
-                    <button onClick={() => addToCart(product)}>Add to cart!</button>
-                    <button onClick={() => { console.log("Remove button clicked for:", product); removeFromCart(product); }}>Remove from cart!</button>
-                </div>
-            ))}
+        <div className="p-4">
+            <h1 className="text-2xl mb-4">Products</h1>
+            <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+                {products.map(product => (
+                    <div key={product.id} className="border p-2 rounded-lg shadow-sm">
+                        <img src={product.images[0]} alt={product.name} className="w-full h-24 object-contain mb-2 rounded" />
+                        <h2 className="text-lg mb-1">{product.name}</h2>
+                        <p className="text-xs text-gray-600 mb-2">{product.description}</p>
+                        <div className="flex gap-2">
+                            <button onClick={() => addToCart(product)} className="bg-blue-500 text-white text-xxs rounded-md hover:bg-blue-600 transition-colors w-24 h-6">Add</button>
+                            <button onClick={() => { console.log("Remove button clicked for:", product); removeFromCart(product); }} className="bg-red-500 text-white text-xxs rounded-md hover:bg-red-600 transition-colors w-24 h-6">Remove</button>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
-    );
+    )
+
+
+
+
+
+        ;
 }
 
 export default Products;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import CartProvider from './context/CartContext.jsx'
 import App from './App.jsx'
+import './main.css'; // Or './main.css' or the path to your CSS file
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
