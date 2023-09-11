5948d290ee24d26296f6ff0e13d889d840d96140
sabest2022 <askarians@me.com>
2023-09-11 21:41:12 +0200

    update organizing

---
+++ server/routes/webhook.route.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const { handleWebhook } = require("../controllers/webhook.controller");
+
+
+
+const webhookRouter = express
+    .Router()
+    .post('/webhook', express.raw({ type: 'application/json' }), handleWebhook)
+
+
+module.exports = { webhookRouter };
+
